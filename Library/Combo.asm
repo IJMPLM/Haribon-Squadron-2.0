e0d5337d86c41a090db63c37982173c5779a81ba
Jieco Lao <jiecoandres.lao07@gmail.com>
2025-05-10 21:53:45 +0800

    Combo visual logic implemented

--- Library/Combo.asm
+++ Library/Combo.asm
@@ -24,22 +24,51 @@ DATASEG
 	CAN_USE_INVINCIBLE db  0    ; Flag if invincibility is available
 	CAN_USE_FREEZE   	db  0    ; Flag if freeze is available
 
-	; Combo BMPs (add 2, 4, 6, 7, 8)
-	Combo1FileName				db	'Assets/9.bmp', 0
+	; Combo BMPs
+	Combo0FileName				db	'Assets/0.bmp', 0
+	Combo0FileHandle			dw	?
+	Combo0Length					equ	16
+	Combo0Height					equ	16
+
+	; Combo1FileName				db	'Assets/1.bmp', 0
 	Combo1FileHandle			dw	?
 	Combo1Length					equ	16
 	Combo1Height					equ	16
 
-	Combo3FileName				db	'Assets/9.bmp', 0
+	; Combo2FileName				db	'Assets/2.bmp', 0
+	Combo2FileHandle			dw	?
+	Combo2Length					equ	16
+	Combo2Height					equ	16
+
+	Combo3FileName				db	'Assets/3.bmp', 0
 	Combo3FileHandle			dw	?
 	Combo3Length					equ	16
 	Combo3Height					equ	16
-	Combo3Height					equ	16
 
-	Combo5FileName				db	'Assets/9.bmp', 0
+	; Combo4FileName				db	'Assets/4.bmp', 0
+	Combo4FileHandle			dw	?
+	Combo4Length					equ	16
+	Combo4Height					equ	16
+
+	Combo5FileName				db	'Assets/5.bmp', 0
 	Combo5FileHandle			dw	?
 	Combo5Length					equ	16
 	Combo5Height					equ	16
+
+	; Combo6FileName				db	'Assets/6.bmp', 0
+	Combo6FileHandle			dw	?
+	Combo6Length					equ	16
+	Combo6Height					equ	16
+
+	; Combo7FileName				db	'Assets/7.bmp', 0
+	Combo7FileHandle			dw	?
+	Combo7Length					equ	16
+	Combo7Height					equ	16
+
+	; Combo8FileName				db	'Assets/8.bmp', 0
+	Combo8FileHandle			dw	?
+	Combo8Length					equ	16
+	Combo8Height					equ	16
 
 	Combo9FileName				db	'Assets/9.bmp', 0
 	Combo9FileHandle			dw	?
@@ -56,35 +85,111 @@ CODESEG
 ;--------------------------------------------------------------------
 
 proc DisplayCombo ; called in Game.asm, search word "#Jieco"
-@printCombo1:
-	push offset Combo1FileName
-	push offset Combo1FileHandle	
+	xor ah, ah
+	mov al, [COMBO_VAL]
+	cmp al, 0
+	je @@NoDisplay
+	cmp al, 1
+	je @@printCombo1
+	cmp al, 2
+	je @@printCombo2
+	cmp al, 3
+	je @@printCombo3
+	cmp al, 4
+	je @@printCombo4
+	cmp al, 5
+	je @@printCombo5
+	cmp al, 6
+	je @@printCombo6
+	cmp al, 7
+	je @@printCombo7
+	cmp al, 8
+	je @@printCombo8
+	cmp al, 9
+	je @@printCombo9
+
+@@NoDisplay:	; should have 0 bmp
+	push offset Combo0FileName
+	push offset Combo0FileHandle	
 	call OpenFile
 
-	push [Combo1FileHandle]
-	push Combo1Length
-	push Combo1Height
+	push [Combo0FileHandle]
+	push Combo0Length
+	push Combo0Height
 	push ComboPrintStartLine
 	push ComboPrintStartRow
 	push offset FileReadBuffer
 	call PrintBMP
 
-	push [Combo1FileHandle]
+	push [Combo0FileHandle]
 	call CloseFile
-
-@printCombo2:
-
 	ret
+
+@@printCombo1:
+	ret
+
+@@printCombo2:
+	ret
+
+@@printCombo3:
+	push offset Combo3FileName
+	push offset Combo3FileHandle	
+	call OpenFile
+
+	push [Combo3FileHandle]
+	push Combo3Length
+	push Combo3Height
+	push ComboPrintStartLine
+	push ComboPrintStartRow
+	push offset FileReadBuffer
+	call PrintBMP
+
+	push [Combo3FileHandle]
+	call CloseFile
+	ret
+
+@@printCombo4:
+	ret
+
+@@printCombo5:
+	push offset Combo5FileName
+	push offset Combo5FileHandle	
+	call OpenFile
+
+	push [Combo5FileHandle]
+	push Combo5Length
+	push Combo5Height
+	push ComboPrintStartLine
+	push ComboPrintStartRow
+	push offset FileReadBuffer
+	call PrintBMP
+
+	push [Combo5FileHandle]
+	call CloseFile
+	ret
+
+@@printCombo6:
+	ret
+
+@@printCombo7:
+	ret
+
+@@printCombo8:
+	ret
+
+@@printCombo9:
+	ret
+
 endp DisplayCombo
 
 ;--------------------------------------------------------------------
-; Updates the combo shown on screen
+; Updates the combo shown on screen (to be hidden)
 ;--------------------------------------------------------------------
 
 proc UpdateComboStat ; called in Game.asm, search word "#Jieco"
 	xor bh, bh
 	mov dh, 19
-	mov dl, 37
+	mov dl, 33
 	mov ah, 2
 	int 10h
 
@@ -156,34 +261,35 @@ endp ResetCombo
 ;--------------------------------------------------------------------
 ; Check which skills are available based on current combo value
 ;--------------------------------------------------------------------
+
 proc CheckSkillAvailability
-    ; Check Regen Heart availability
-    mov al, [COMBO_VAL]
-    cmp al, REGEN_COST
-    jae @@canRegen
-    mov [byte ptr CAN_USE_REGEN], 0
-    jmp @@checkInvincible
+	; Check Regen Heart availability
+	mov al, [COMBO_VAL]
+	cmp al, REGEN_COST
+	jae @@canRegen
+	mov [byte ptr CAN_USE_REGEN], 0
+	jmp @@checkInvincible
 @@canRegen:
-    mov [byte ptr CAN_USE_REGEN], 1
+	mov [byte ptr CAN_USE_REGEN], 1
 
 @@checkInvincible:
-    mov al, [COMBO_VAL]
-    cmp al, INVINCIBLE_COST
-    jae @@canInvincible
-    mov [byte ptr CAN_USE_INVINCIBLE], 0
-    jmp @@checkFreeze
+	mov al, [COMBO_VAL]
+	cmp al, INVINCIBLE_COST
+	jae @@canInvincible
+	mov [byte ptr CAN_USE_INVINCIBLE], 0
+	jmp @@checkFreeze
 @@canInvincible:
-    mov [byte ptr CAN_USE_INVINCIBLE], 1
+	mov [byte ptr CAN_USE_INVINCIBLE], 1
 
 @@checkFreeze:
-    mov al, [COMBO_VAL]
-    cmp al, FREEZE_COST
-    jae @@canFreeze
-    mov [byte ptr CAN_USE_FREEZE], 0
-    jmp @@endCheck
+	mov al, [COMBO_VAL]
+	cmp al, FREEZE_COST
+	jae @@canFreeze
+	mov [byte ptr CAN_USE_FREEZE], 0
+	jmp @@endCheck
 @@canFreeze:
-    mov [byte ptr CAN_USE_FREEZE], 1
+	mov [byte ptr CAN_USE_FREEZE], 1
 
 @@endCheck:
-    ret
+	ret
 endp CheckSkillAvailability
